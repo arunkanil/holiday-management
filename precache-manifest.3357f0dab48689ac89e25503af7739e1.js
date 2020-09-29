a902f7e9a42b46653d848c990a86f5c765b42d2c
Arun <arun@untanglestrategy.com>
2020-09-29 12:48:24 +0530

    Updates

---
+++ precache-manifest.3357f0dab48689ac89e25503af7739e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeaeded15fafe66883ef2ab8fe2fdcf",
+    "url": "/holiday-management/index.html"
+  },
+  {
+    "revision": "82804d686a82ba947463",
+    "url": "/holiday-management/static/css/2.07bbe244.chunk.css"
+  },
+  {
+    "revision": "36bf5b91b84a864d08b3",
+    "url": "/holiday-management/static/css/main.658c8fc5.chunk.css"
+  },
+  {
+    "revision": "82804d686a82ba947463",
+    "url": "/holiday-management/static/js/2.7c5fe7ef.chunk.js"
+  },
+  {
+    "revision": "23970fbe20904b9641d04c74c2b758fb",
+    "url": "/holiday-management/static/js/2.7c5fe7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36bf5b91b84a864d08b3",
+    "url": "/holiday-management/static/js/main.6dd3899e.chunk.js"
+  },
+  {
+    "revision": "8b7ef4aa756a756099d7",
+    "url": "/holiday-management/static/js/runtime-main.c28b8397.js"
+  },
+  {
+    "revision": "1f9bf38838dafddcb2bf181fdb3c9701",
+    "url": "/holiday-management/static/media/caret.1f9bf388.svg"
+  },
+  {
+    "revision": "2999d7dbc9597d512bebbe85cc796275",
+    "url": "/holiday-management/static/media/deleteIcon.2999d7db.svg"
+  },
+  {
+    "revision": "2ab221362ac37906a8c6d998093d509e",
+    "url": "/holiday-management/static/media/edit.2ab22136.svg"
+  },
+  {
+    "revision": "348dee448f78d73d826c49b0c1e89f6e",
+    "url": "/holiday-management/static/media/logo.348dee44.png"
+  },
+  {
+    "revision": "f912f9ada6d8ab1028f8863de0fe8ac4",
+    "url": "/holiday-management/static/media/logout.f912f9ad.svg"
+  }
+]);
